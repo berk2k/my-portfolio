e7a6a91480fb446e6919c38fb2c0a35187bc4ed1
Berk Polat <berkpolat125@gmail.com>
2025-08-27 13:21:35 +0300

    order changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,9 @@ const Navbar = () => {
 
   const links = [
     { name: "Home", href: "#home" },
-    { name: "Skills", href: "#skills" },
-    { name: "Experience", href: "#experience" },
     { name: "Projects", href: "#projects" },
+    { name: "Experience", href: "#experience" },
+    { name: "Skills", href: "#skills" },
     { name: "Contact", href: "#contact" },
   ];
 
