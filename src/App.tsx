d5d823991eee9ecbda74d74e93d56de6d6815aa8
Berk Polat <berkpolat125@gmail.com>
2025-08-27 11:33:38 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-neutral-900">
+      <h1 className="text-4xl font-bold text-white underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
